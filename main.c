12b5c3ae62b2c86bd0778e51ddb19abdd80edea6
Korna <artem-koma@yandex.ru>
2016-05-20 22:52:49 +0600

    First implement of program

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+void main() {
+	float a, b, c;
+	printf("Input a:\n");
+	scanf("%f", &a);
+	printf("Input b:\n");
+	scanf("%f", &b);
+	printf("Input c:\n");
+	scanf("%f", &c);
+	funccalc(a, b, c);
+	getchar();
+	getchar();
+}
